fd1c21dbb4074dd4f3be90fcbcdb1aa3641bc463
MarioQL <mquiros@epromcr.com>
2025-01-09 12:11:23 -0600

    Creacion microservicio para manejo de usuarios

---
+++ src/test/java/com/example/springcloud/microservicio/users/microservicio_users/MicroservicioUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.microservicio.users.microservicio_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
